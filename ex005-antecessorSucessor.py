1884ee8748f188da839d69d93ff1bc19862f5e82
Kaio César Barreto <kaio.barreto@academico.ufpb.br>
2022-10-14 20:07:50 -0300

    Upload ex005-antecessorSucessor.py
    This program asks for an integer and says its predecessor and successor.

---
+++ ex005-antecessorSucessor.py
@@ -0,0 +1,4 @@
+#This program asks for an integer and says its predecessor and successor
+print('\nEsse programa pede um número inteiro e diz seu antecessor e sucessor')
+numero = int(input('Digite um número: '))
+print('\n{} < {} < {}'.format(numero-1, numero, numero+1))
