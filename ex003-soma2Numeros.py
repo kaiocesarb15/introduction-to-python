acbe7125201f987931f97c50b9f8428753b99476
Kaio César Barreto <kaio.barreto@academico.ufpb.br>
2022-10-14 20:03:44 -0300

    Upload ex003-soma2Numeros.py
    This program must perform the sum of two numbers.

---
+++ ex003-soma2Numeros.py
@@ -0,0 +1,6 @@
+#This program must perform the sum of two any numbers
+print('\nEsse programa deve realizar a soma de dois números quaisquer')
+numero1 = int(input('Por favor digite um número: '))
+numero2 = int(input('Por favor digite outro número: '))
+print('\n{} + {} = {}'.format(numero1, numero2, numero1+numero2))
+
