7d5602e46ee2517e8b53ce63ecb64d22de55cf78
Kaio César Barreto <kaio.barreto@academico.ufpb.br>
2022-10-13 21:50:21 -0300

    Upload ex002-respondendoUsuario.py
    This program should ask your name and use it in a welcome phrase

---
+++ ex002-respondendoUsuario.py
@@ -0,0 +1,4 @@
+#This program should ask your name and use it in a welcome phrase
+print('\nEsse programa deve perguntar seu nome e utilizar ele em uma frase de boas vindas')
+nome = input('Olá, qual o seu nome? ')
+print('\nOi {}! prazer em te conhecer'. format(nome))
