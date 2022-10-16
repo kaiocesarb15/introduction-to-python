f754e1a95f4b341b6639a267fa91f812c8961648
Kaio César Barreto <kaio.barreto@academico.ufpb.br>
2022-10-16 19:11:05 -0300

    Upload ex007-mediaAritmetica.py
    This program averages two values.

---
+++ ex007-mediaAritmetica.py
@@ -0,0 +1,5 @@
+#This program averages two values
+print('\nEsse programa calcula a média de dois valores')
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+print('\nA média é {:.1f}'.format((n1+n2)/2))
