e4472e3a2e11194cff7eff6e3b1fb67868f31051
Kaio César Barreto <kaio.barreto@academico.ufpb.br>
2022-10-25 22:12:44 -0300

    Upload ex012-calculandoDescontos.py
    This program receives a value and applies a 5% discount on this amount.

---
+++ ex012-calculandoDescontos.py
@@ -0,0 +1,4 @@
+#This program receives a value and applies a 5% discount on this amount
+print('\nEsse programa recebe um valor e aplica um desconto de 5% nesse valor')
+valor = float(input('Digite um valor? '))
+print('\nO seu novo valor é {:.1}'.format(valor*0.95))
